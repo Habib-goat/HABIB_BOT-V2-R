1ca3316d4c46736aaa1e364d55d8ee53b73544f6
Riyad761 <64275345+Riyad761@users.noreply.github.com>
2026-07-09 00:33:11 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -18,15 +18,31 @@ module.exports = {
   },
 
   onStart: async function({ api, event }) {
-    let ownerInfo = `👤 ━━━━ [ RIYAD BOT DEVELOPER ] ━━━━ 👤\n\n`;
-    ownerInfo += `👑 Main Developer: Hasan Riyad\n`;
-    ownerInfo += `🌐 Github: https://github.com/namebdmy/Riyad_Pro/\n`;
-    ownerInfo += `📧 Contact: hasanriyad761@gmail.com\n`;
-    ownerInfo += `💬 Messenger ID: ${config.ownerIDs[0]}\n`;
-    ownerInfo += `💡 Framework: Riyad Bot Framework V1.0.0\n`;
-    ownerInfo += `📜 License: MIT Open Source\n\n`;
-    ownerInfo += `Thank you for choosing Riyad Bot! Build your own plugins or commands easily with our GoatBot compatibility layers.`;
+  const ownerInfo = `‎‎╔═ ❖👑 𝑶𝑾𝑵𝑬𝑹 𝑰𝑵𝑭𝑶 👑❖ ═╗
 
-    await api.sendMessage(ownerInfo, event.threadID);
-  }
-};
+❖ 👤 𝑵𝒂𝒎𝒆        ⟿ 𝑩𝒂𝒅 𝑩𝒐𝒚 𝑹𝒊𝒚𝒂𝒅
+❖ 🧸 𝑵𝒊𝒄𝒌          ⟿ 𝑹𝒊𝒚𝒂𝒅
+❖ 🎂 𝑨𝒈𝒆            ⟿ 18+
+❖ 💘 𝑹𝒆𝒍𝒂𝒕𝒊𝒐𝒏     ⟿ 𝑺𝒊𝒏𝒈𝒍𝒆
+❖ 🎓 𝑷𝒓𝒐𝒇𝒆𝒔𝒔𝒊𝒐𝒏  ⟿ 𝑺𝒕𝒖𝒅𝒆𝒏𝒕
+❖ 📚 𝑬𝒅𝒖𝒄𝒂𝒕𝒊𝒐𝒏  ⟿ 𝑰𝒏𝒕𝒆𝒓 2𝒏𝒅 𝒀𝒆𝒂𝒓
+❖ 🏡 𝑳𝒐𝒄𝒂𝒕𝒊𝒐𝒏    ⟿ 𝑩𝒐𝒈𝒖𝒓𝒂 • 𝑺𝒉𝒆𝒓𝒑𝒖𝒓
+
+╠═══ 🔗 𝑪𝑶𝑵𝑻𝑨𝑪𝑻 🔗 ═══╣
+
+📘 𝑭𝒂𝒄𝒆𝒃𝒐𝒐𝒌   ➜ fb.com/badboyriyad
+💬 𝑰𝒏𝒔𝒕𝒂𝒈𝒓𝒂𝒎 ➜ insta.com/chocoriyad
+📞 𝑾𝒉𝒂𝒕𝒔𝑨𝒑𝒑 ➜ wa.me/01863691054
+
+━━━━━━━━━━━━━━━━━━━━━━
+
+🌐 GitHub      ➜ https://github.com/namebdmy/Riyad_Pro/
+📧 Email       ➜ hasanriyad761@gmail.com
+💬 Messenger ID ➜ ${config.ownerIDs[0]}
+💡 Framework   ➜ Riyad Bot Framework V1.0.0
+📜 License     ➜ MIT Open Source
+
+╚══ ❖ 💎 𝑻𝒉𝒂𝒏𝒌 𝒀𝒐𝒖 💎 ❖ ══╝`;
+
+  await api.sendMessage(ownerInfo, event.threadID);
+}
