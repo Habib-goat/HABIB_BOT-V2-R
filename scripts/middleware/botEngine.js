197d95b49920b79c6d5e77ca92d000345c4f1383
Riyad761 <64275345+Riyad761@users.noreply.github.com>
2026-07-12 00:28:05 +0600

    Update botEngine.js

--- scripts/middleware/botEngine.js
+++ scripts/middleware/botEngine.js
@@ -206,7 +206,7 @@ if (config.autoReply.enabled) {
       const tempPath = path.join(os.tmpdir(), `prefix_${Date.now()}.gif`);
 
       const response = await axios({
-        url: "https://files.catbox.moe/uzw5yu.gif",
+        url: "https://files.catbox.moe/kmqwfy.gif",
         method: "GET",
         responseType: "stream"
       });
