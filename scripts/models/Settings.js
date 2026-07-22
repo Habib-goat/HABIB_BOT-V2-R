9d03f82df21b5ddba6864a9c5bfded48d83a86aa
Riyad761 <64275345+Riyad761@users.noreply.github.com>
2026-07-22 11:39:21 +0600

    Update Settings.js

--- scripts/models/Settings.js
+++ scripts/models/Settings.js
@@ -1,1 +1,26 @@
+const mongoose = require("mongoose");
 
+const settingsSchema = new mongoose.Schema(
+  {
+    totalCommandsExecuted: {
+      type: Number,
+      default: 0
+    },
+
+    blockedUsers: {
+      type: Map,
+      of: Boolean,
+      default: {}
+    },
+
+    systemUptimeStart: {
+      type: Number,
+      default: Date.now
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Settings", settingsSchema);
