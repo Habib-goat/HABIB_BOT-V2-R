d210ca9fd3118b35feb3ea0a4ab9b39ab60823e6
Riyad761 <64275345+Riyad761@users.noreply.github.com>
2026-07-22 11:38:12 +0600

    Update User.js

--- scripts/models/User.js
+++ scripts/models/User.js
@@ -1,1 +1,57 @@
+const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true
+    },
+
+    name: {
+      type: String,
+      default: "Unknown User"
+    },
+
+    exp: {
+      type: Number,
+      default: 0
+    },
+
+    level: {
+      type: Number,
+      default: 1
+    },
+
+    money: {
+      type: Number,
+      default: 500
+    },
+
+    bank: {
+      type: Number,
+      default: 0
+    },
+
+    lastDaily: {
+      type: Number,
+      default: 0
+    },
+
+    banned: {
+      type: Boolean,
+      default: false
+    },
+
+    inventory: {
+      type: [mongoose.Schema.Types.Mixed],
+      default: []
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
