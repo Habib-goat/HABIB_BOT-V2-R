de3cec5aaa9af72390480bd979cc062925979f7c
Riyad761 <64275345+Riyad761@users.noreply.github.com>
2026-07-25 08:14:34 +0600

    Update baby.js

--- scripts/cmds/baby.js
+++ scripts/cmds/baby.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const baseApiUrl = async () => "https://noobs-api.top/dipto/baby";
+const baseApiUrl = async () => "https://simsimi-api-tjb1.onrender.com";
 
 module.exports.config = {
  name: "baby",
