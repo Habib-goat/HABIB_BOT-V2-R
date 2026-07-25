e1fc14a9c649216ee6e84b6533cb46559980af01
Riyad761 <64275345+Riyad761@users.noreply.github.com>
2026-07-25 16:55:26 +0600

    Create callwelcome.js

---
+++ scripts/events/callwelcome.js
@@ -0,0 +1,14 @@
+module.exports = {
+  config: {
+    name: "callWelcome",
+    eventType: [
+      // এখানে Call Event-এর নাম থাকবে
+    ],
+    version: "1.0.0",
+    author: "Riyad"
+  },
+
+  onStart: async function ({ api, event, threadsData, usersData }) {
+    // Call Join Event Handle হবে
+  }
+};
