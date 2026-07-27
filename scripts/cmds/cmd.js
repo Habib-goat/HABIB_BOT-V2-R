e0b8cc9b0fb0c78b4d7e8846fb8c36cd2af7c150
Riyad761 <64275345+Riyad761@users.noreply.github.com>
2026-07-27 15:11:06 +0600

    Update cmd.js

--- scripts/cmds/cmd.js
+++ scripts/cmds/cmd.js
@@ -177,7 +177,7 @@ module.exports = {
   config: {
     name: "cmd",
     aliases: ["cmdman", "command"],
-    version: "2.0.0",
+    version: "2.0.1",
     author: "Riyad",
     role: 2,
     category: "system",
