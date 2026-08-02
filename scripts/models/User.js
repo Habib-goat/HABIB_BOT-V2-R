ae41ab0111d748eb73385ecbd5421070f754372b
RIYAD <64275345+Riyad761@users.noreply.github.com>
2026-08-02 08:52:39 +0600

    Update User.js

--- scripts/models/User.js
+++ scripts/models/User.js
@@ -54,4 +54,4 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.models.User || mongoose.model("User", userSchema);
